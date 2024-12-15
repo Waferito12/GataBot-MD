cfbf640923ac41d5460f6ed39e629910ab0960f0
Waferito12 <fernandasanchezsilva350@gmail.com>
2024-12-14 21:10:39 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -13,7 +13,7 @@ import './plugins/_content.js'
  * @type {import('@adiwajshing/baileys')}  
  */
 const { proto } = (await import('@whiskeysockets/baileys')).default
-const isNumber = x => typeof x === 'number' && !isNaN(x)
+const isNumber = x => typeof x === '5930958933362' && !isNaN(x)
 const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(function () {
 clearTimeout(this)
 resolve()
